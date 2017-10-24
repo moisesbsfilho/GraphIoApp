a9ec4efb7f8dd6a01e10fbaab1ad19620d745197
Moises Batista <MBatista@devrygroup.com>
2017-10-24 16:14:04 -0300

    Configured Apollo client

---
+++ mobile/GraphIo/src/app/apollo-client.ts
@@ -0,0 +1,14 @@
+import {ApolloClient, createNetworkInterface} from 'apollo-client';
+
+const client = new ApolloClient({
+    networkInterface: createNetworkInterface({
+        uri:'/graphql',
+        opts: {
+            credentials: 'same-origin'
+        },
+    }),
+});
+
+export function getClient(): ApolloClient{
+    return client;
+}
--- mobile/GraphIo/src/app/app.module.ts
+++ mobile/GraphIo/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
+import { ApolloModule } from 'apollo-angular';
+import { getClient } from './apollo-client';
 
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
@@ -21,7 +23,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    ApolloModule.withClient(getClient),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
