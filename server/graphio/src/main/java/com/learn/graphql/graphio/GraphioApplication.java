903e38065686b1a7921b269c6fcb9e52dbf679bc
Moises Batista <MBatista@devrygroup.com>
2017-10-24 15:07:57 -0300

    First commit

---
+++ server/graphio/src/main/java/com/learn/graphql/graphio/GraphioApplication.java
@@ -0,0 +1,12 @@
+package com.learn.graphql.graphio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphioApplication.class, args);
+	}
+}
